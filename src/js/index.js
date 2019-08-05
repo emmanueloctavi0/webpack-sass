c0390d2a72282c68f2913c85ae545167442d8daa
Emmanuel Octavio Martínez Calderón <emmanueloctaviomc@gmail.com>
2019-08-05 13:26:46 -0500

    Se modifica el componente content

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,8 @@ const content = require('../components/content.handlebars');
 
 document.addEventListener('DOMContentLoaded', () => {
 	document.body.innerHTML = navbar();
-	document.body.innerHTML += content();
+	document.body.innerHTML += content({
+		title: 'Contenido',
+		content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat reprehenderit error rem quis? Tempora quidem obcaecati facilis earum mollitia ad, sed labore veniam, itaque corrupti aliquam, enim nulla impedit nam?'
+	});
 });
