73dbd81a0f03ad4e8f3cc99203a0ef9cd830a370
Emmanuel Octavio Martínez Calderón <emmanueloctaviomc@gmail.com>
2019-08-05 09:38:42 -0500

    Se agrega un navbar y un content

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
-console.log('Hola')
+
+require('../scss/index.scss');
+
+const navbar = require('../components/navbar.handlebars');
+const content = require('../components/content.handlebars');
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.body.innerHTML = navbar();
+	document.body.innerHTML += content();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -7,7 +8,20 @@ module.exports = {
     contentBase: './dist'
   },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        "style-loader", // creates style nodes from JS strings
+        "css-loader", // translates CSS into CommonJS
+        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+      ]
+      },
+      { test: /\.handlebars$/, loader: 'handlebars-loader' }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin]
 };
